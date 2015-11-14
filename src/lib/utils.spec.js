8125c2edf40337490647597d69121e2d524348f1
dragosh-bb <dragos@backbase.com>
2015-11-14 12:41:36 +0100

    small typo

--- src/lib/utils.spec.js
+++ src/lib/utils.spec.js
@@ -3,7 +3,7 @@ import { expect, should } from 'chai'
 import { sum } from './utils'
 
 describe('Utils sum', () => {
-    it('should sum with on argument', () => {
+    it('should sum with one argument', () => {
         expect(sum(2)).to.equal(2);
     })
     it('should sum with multiple arguments', () => {
